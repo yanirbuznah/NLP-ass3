474068de63d0ba139b5216a45a49712971bcf734
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2022-05-13 13:06:28 +0300

    add all three of the types in main using pandas

--- counter.py
+++ counter.py
@@ -26,57 +26,108 @@ def read_table_from_file(file_name):
     df.columns = ['ID', 'FORM', 'LEMMA', 'COARSE-POS', 'FINE-POS', '-', 'HEAD', 'DEP-TYPE', '-', '-']
     print(df.shape)
     print(df.head())
+    return df
 
 
 def count_words(sentences):
     words = {}
+    idx_to_word = {}
+    word_to_idx = {}
+
     for sentence in sentences:
         for word in sentence:
-            if word in words:
-                words[word] += 1
+            idx = get_idx_from_dicts(idx_to_word, word_to_idx, word)
+            if idx in words:
+                words[idx] += 1
             else:
-                words[word] = 1
-    return words
+                words[idx] = 1
+    return words, idx_to_word, word_to_idx
 
 
-def write_count_to_file(file_name, words_counts, k=50):
+def write_count_to_file(file_name, words_counts, idx_to_word, k=50):
     words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
     with open(file_name, 'w', encoding='utf-8') as f:
-        for word, count in words[:k]:
-            f.write(word + ' ' + str(count) + '\n')
+        for idx, count in words[:k]:
+            f.write(idx_to_word[idx] + ' ' + str(count) + '\n')
 
 
-def context_counter(sentences):
+def get_idx_from_dicts(idx_to_word, word_to_idx, word):
+    if word not in word_to_idx.keys():
+        word_to_idx[word] = len(word_to_idx)
+        idx_to_word[len(idx_to_word)] = word
+    return word_to_idx[word]
+
+
+def context_counter(sentences, window=2):
     counts = defaultdict(Counter)
+    idx_to_word = {}
+    word_to_idx = {}
     for s in sentences:
-        for w in s:
-            counts[w] = Counter() if w not in counts else counts[w]
-            for w2 in s:
-                if w != w2:
-                    counts[w][w2] = 1 if w2 not in counts[w] else counts[w][w2] + 1
+        for i, w in enumerate(s):
+            idx = get_idx_from_dicts(idx_to_word, word_to_idx, w)
+            if idx not in counts.keys():
+                counts[idx] = Counter()
+            count_context_in_range(counts, word_to_idx, idx_to_word, s, i, window)
 
-    return counts
+    return counts, idx_to_word, word_to_idx
+
+
+def count_context_in_range(counts, word_to_idx, idx_to_word, s, index, window=2):
+    start = max(0, index - window)
+    end = min(len(s), index + window + 1)
+
+    word_idx = word_to_idx[s[index]]
+    for j in range(start, index):
+        context_idx = get_idx_from_dicts(idx_to_word, word_to_idx, s[j])
+        counts[word_idx][context_idx] = 1 if context_idx not in counts[word_idx] else counts[word_idx][context_idx] + 1
+
+    for j in range(index + 1, end):
+        context_idx = get_idx_from_dicts(idx_to_word, word_to_idx, s[j])
+        counts[word_idx][context_idx] = 1 if context_idx not in counts[word_idx] else counts[word_idx][context_idx] + 1
+
+
+def pandas_tries(file_name):
+    df = read_table_from_file(file_name)
+    counts = df['LEMMA'].value_counts()
+    starts_indexes = df.index[df['ID'] == 1].tolist() + [len(df)]
+    list_of_sentences = [df.iloc[starts_indexes[n]:starts_indexes[n + 1]] for n in range(len(starts_indexes) - 1)]
+
+
+
+
 
 def main():
     file_name = sys.argv[1]
     # read lemma sentences from file
     sentences = read_file(file_name)
 
-    # read lemma sentences from file
-    # sentences = read_table_from_file(file_name)
+    # # read lemma sentences from file
+    pandas_tries(file_name)
+    # df = read_table_from_file(file_name)
+    # counts = df['LEMMA'].value_counts()
+    # starts_indexes = df.index[df['ID'] == 1].tolist() + [len(df)]
+    # list_of_sentences = [df.iloc[starts_indexes[n]:starts_indexes[n+1]] for n in range(len(starts_indexes)-1)]
 
     # count words
-    words = count_words(sentences)
+    words, idx_to_word, word_to_idx = count_words(sentences)
 
     # write words to file
-    write_count_to_file('words.txt', words)
+    write_count_to_file('words.txt', words, idx_to_word)
 
     # count context
-    context = context_counter(sentences)
+    context, idx_to_word, word_to_idx = context_counter(sentences, window=sys.maxsize)
 
     # write context to file
-    context_list = {k:sum(v.values()) for k,v in context.items()}
-    write_count_to_file('context.txt', context_list)
+    context_list = {k: sum(v.values()) for k, v in context.items()}
+    write_count_to_file('context_max_window.txt', context_list, idx_to_word)
+
+    # count context
+    context, idx_to_word, word_to_idx = context_counter(sentences, window=2)
+
+    # write context to file
+    context_list = {k: sum(v.values()) for k, v in context.items()}
+    write_count_to_file('context_window2.txt', context_list, idx_to_word)
+
 
 if __name__ == '__main__':
     main()
---
+++ main.py
@@ -0,0 +1,114 @@
+import sys
+from collections import defaultdict, Counter
+
+import numpy as np
+import pandas as pd
+
+
+def read_table_from_file(file_name):
+    df = pd.read_csv(file_name, sep='\t', header=None)
+    df.columns = ['ID', 'FORM', 'LEMMA', 'COARSE-POS', 'FINE-POS', '-', 'HEAD', 'DEP-TYPE', '-', '-']
+    # df.drop(set(df.columns) - {'ID', 'FORM', 'LEMMA', 'HEAD'}, axis=1, inplace=True)
+    df.drop(['COARSE-POS', 'FINE-POS', '-', '-', '-'], axis=1, inplace=True)
+    print(df.shape)
+    print(df.head())
+    return df
+
+
+def count_context_by_dep(counts, word_to_idx, s, index):
+    word_idx = word_to_idx[s['LEMMA'][index]]
+
+    dep_type = s['DEP-TYPE'][index]
+
+    #TODO: check if this is correct behavior
+    if dep_type == 'ROOT':
+        return
+
+    head = s['LEMMA'][s['HEAD'][index]]
+    head_idx = word_to_idx[head]
+    t = (head_idx, 1, dep_type)
+    if t not in counts[word_idx].keys():
+        counts[word_idx][t] = 1
+    else:
+        counts[word_idx][t] += 1
+    t = (word_idx, -1, dep_type)
+
+    if t not in counts[head_idx].keys():
+        counts[head_idx][t] = 1
+    else:
+        counts[head_idx][t] += 1
+
+
+def context_counter(sentences, words_to_idx, idx_to_words, task='full_window'):
+    counts = defaultdict(Counter)
+    # context_counter = Counter(idx_to_words)
+    # for i in context_counter.keys():
+    #     context_counter[i] = 0
+    for s in sentences:
+        s.index = np.arange(1, len(s) + 1)
+        for i, w in enumerate(s['LEMMA']):
+            # if w not in counts.keys():
+            #     counts[w] = Counter()
+            if task == 'full_window':
+                count_context_in_range(counts, words_to_idx, s['LEMMA'], i + 1, sys.maxsize)
+            elif task == '2_window':
+                count_context_in_range(counts, words_to_idx, s['LEMMA'], i + 1, 2)
+            else:
+                count_context_by_dep(counts, words_to_idx, s, i + 1)
+
+    return counts
+
+
+def count_context_in_range(counts, word_to_idx, s, index, window=2):
+    start = max(1, index - window)
+    end = min(len(s) + 1, index + window + 1)
+    # s = s.LEMMA
+    word_idx = word_to_idx[s[index]]
+    for j in range(start, index):
+        context_idx = word_to_idx[s[j]]
+        counts[word_idx][context_idx] = 1 if context_idx not in counts[word_idx] else counts[word_idx][context_idx] + 1
+
+    for j in range(index + 1, end):
+        context_idx = word_to_idx[s[j]]
+        counts[word_idx][context_idx] = 1 if context_idx not in counts[word_idx] else counts[word_idx][context_idx] + 1
+
+
+#
+# def count_context_in_range(context_counter, word_to_idx, s, index, window=2):
+#     start = max(1, index - window)
+#     end = min(len(s), index + window + 1)
+#
+#     for j in range(start, index):
+#         # context_idx = word_to_idx[s[j]]
+#         context_counter[s['LEMMA'][j]] += 1
+#         counts[word_idx][context_idx] = 1 if context_idx not in counts[word_idx] else counts[word_idx][context_idx] + 1
+#
+#
+#     for j in range(index + 1, end):
+#         # context_idx = word_to_idx[s[j]]
+#         context_counter[s['LEMMA'][j]] += 1
+
+def main():
+    file_name = sys.argv[1]
+    df = read_table_from_file(file_name)
+    counts = df['LEMMA'].value_counts()
+    starts_indexes = df.index[df['ID'] == 1].tolist() + [len(df)]
+    list_of_sentences = [df.iloc[starts_indexes[n]:starts_indexes[n + 1]] for n in range(len(starts_indexes) - 1)]
+    idx_to_words = df.LEMMA.unique()
+    words_to_idx = {word: idx for idx, word in enumerate(idx_to_words)}
+
+    counts = context_counter(list_of_sentences, words_to_idx, idx_to_words, task='full_window')
+    context_list = {k: sum(v.values()) for k, v in counts.items()}
+    context_list = sorted(context_list.items(), key=lambda x: x[1], reverse=True)
+
+    counts = context_counter(list_of_sentences, words_to_idx, idx_to_words, task='2_window')
+    context_list2 = {k: sum(v.values()) for k, v in counts.items()}
+    context_list2 = sorted(context_list2.items(), key=lambda x: x[1], reverse=True)
+
+    counts = context_counter(list_of_sentences, words_to_idx, idx_to_words, task='dep_type')
+    context_list2 = {k: sum(v.values()) for k, v in counts.items()}
+    context_list2 = sorted(context_list2.items(), key=lambda x: x[1], reverse=True)
+
+
+if __name__ == '__main__':
+    main()
