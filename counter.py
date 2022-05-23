f3280d2f7cccabf94fc8853e4b89be4ee58cc6e4
DanielKnafel <52681699+DanielKnafel@users.noreply.github.com>
2022-05-23 22:23:01 +0300

    added w2v and fixed alot of things

--- counter.py
+++ counter.py
@@ -104,13 +104,18 @@ FUNCTION_WORDS = {
     "she",
 }
 
+def write_features_count_to_file(file_name, words_counts, idx_to_word, k=50):
+    words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
+    directions = {1:"U", -1:"D"}
+    with open(file_name, "w", encoding="utf-8") as f:
+        for (idx,direction,dep_type), count in words[:k]:
+            f.write(f"{idx_to_word[idx]}_{directions[direction]}_{dep_type} {str(count)}\n")
 
 def write_count_to_file(file_name, words_counts, idx_to_word, k=50):
     words = sorted(words_counts.items(), key=lambda x: x[1], reverse=True)
     with open(file_name, "w", encoding="utf-8") as f:
         for idx, count in words[:k]:
-            f.write(idx_to_word[idx] + " " + str(count) + "\n")
-
+            f.write(f"{idx_to_word[idx]} {str(count)}\n")
 
 def read_table_from_file(file_name):
     df = pd.read_csv(file_name, sep="\t", header=None, na_filter=False)
@@ -132,9 +137,7 @@ def read_table_from_file(file_name):
     print(df.head())
     return df
 
-
-def count_context_by_dep(counts, context, word_to_idx, s, index, throw=None):
-    global COUNT
+def count_context_by_dep(counts, context, word_to_idx, s, index):
     word_idx = word_to_idx[s["LEMMA"][index]]
     dep_type = s["DEP-TYPE"][index]
 
@@ -143,18 +146,19 @@ def count_context_by_dep(counts, context, word_to_idx, s, index, throw=None):
         return
 
     head = s["LEMMA"][s["HEAD"][index]]
+    if head not in word_to_idx:
+        return
     head_idx = word_to_idx[head]
 
     # find the row where the head is the index
-
     if s["FINE-POS"][index] == "IN":
         prep_is_head = s[s["HEAD"] == index]
         if len(prep_is_head) == 0:
-            # print('no prep_is_head {}'.format(COUNT))
             return
-
-        context[head_idx] += len(prep_is_head)
+        context[(head_idx, 1, (dep_type, s["LEMMA"][index]))] += len(prep_is_head)
         for i, row in prep_is_head.iterrows():
+            if row["LEMMA"] not in word_to_idx:
+                continue
             prep_is_head_idx = word_to_idx[row["LEMMA"]]
             counts[prep_is_head_idx][(head_idx, 1, (dep_type, s["LEMMA"][index]))] += 1
 
@@ -164,52 +168,46 @@ def count_context_by_dep(counts, context, word_to_idx, s, index, throw=None):
         ]
         if len(prep_is_head_idx) == 0:
             return
+        if prep_is_head_idx.iloc[0]["LEMMA"] not in word_to_idx:
+            return
         prep_is_head_idx = word_to_idx[prep_is_head_idx.iloc[0]["LEMMA"]]
         counts[head_idx][(prep_is_head_idx, -1, (dep_type, s["LEMMA"][index]))] += 1
-        context[prep_is_head_idx] += 1
+        context[(prep_is_head_idx, -1, (dep_type, s["LEMMA"][index]))] += 1
     else:
         counts[word_idx][(head_idx, 1, dep_type)] += 1
         counts[head_idx][(word_idx, -1, dep_type)] += 1
-        context[head_idx] += 1
-        context[word_idx] += 1
+        context[(head_idx, 1, dep_type)] += 1
+        context[(word_idx, -1, dep_type)] += 1
 
-    # context[word_idx] += 1
-    # context[head_idx] += 1
-
-
-def context_counter(sentences, words_to_idx, task="full_window"):
+def context_counter(sentences, words, words_to_idx, task="full_window"):
     counts = defaultdict(Counter)
     context = Counter()
     for s in sentences:
+        # used for dep contexts
         s.index = np.arange(1, len(s) + 1)
         for i, w in enumerate(s["LEMMA"]):
+            # only calculate contexts for common words
+            if w not in words_to_idx or words_to_idx[w] not in words.keys():
+                continue
+
             if task == "full_window":
                 count_context_in_range(counts, context, words_to_idx, s["LEMMA"], i + 1, sys.maxsize)
             elif task == "2_window":
-                count_context_in_range(
-                    counts,
-                    context,
-                    words_to_idx,
-                    s["LEMMA"],
-                    i + 1,
-                    2,
-                    ignore_function_words=True,
-                )
+                count_context_in_range(counts, context, words_to_idx, s["LEMMA"], i + 1, 2, ignore_function_words=True)
             else:
                 count_context_by_dep(counts, context, words_to_idx, s, i + 1)
     return counts, context
 
-
 def count_context_in_range(counts, context, word_to_idx, s, index, window=2, ignore_function_words=False):
     start = max(1, index - window)
     end = min(len(s) + 1, index + window)
-    # s = s.LEMMA
+
     word_idx = word_to_idx[s[index]]
     j = index - 1
     while j >= start and j > 0:
         if ignore_function_words and s[j] in FUNCTION_WORDS:
             start -= 1
-        else:
+        elif s[j] in word_to_idx:
             context_idx = word_to_idx[s[j]]
             counts[word_idx][context_idx] += 1
             context[context_idx] += 1
@@ -220,31 +218,33 @@ def count_context_in_range(counts, context, word_to_idx, s, index, window=2, ign
     while j <= end and j <= len(s):
         if ignore_function_words and s[j] in FUNCTION_WORDS:
             end += 1
-        else:
+        elif s[j] in word_to_idx:
             context_idx = word_to_idx[s[j]]
             counts[word_idx][context_idx] += 1
             context[context_idx] += 1
         j += 1
 
-
-def vectorize_counts(context_counts, words, context_list, threshold=100):
+def vectorize_counts(context_counts, words, context_list, word_to_idx, threshold=100, dep=False):
     vectors = []
     # iterate over all words
     for idx in words.keys():
+        # counts = combine_dep_contexts(context_counts) if dep else context_counts
         # sort the context counts by frequency
         counts = sorted(context_counts[idx].items(), key=lambda x: x[1], reverse=True)
-
         # create a vector of zeros and trim the counts to the threshold (100)
         counts = counts[:threshold]
-        vector = np.zeros(len(words))
-
+        # vector = np.zeros(len(words))
+        vector = np.zeros(len(word_to_idx))
+        # f = filter(lambda x: x[0] in words.keys(), counts) if not dep else filter(lambda x: x[0][0] in words.keys(), counts)
+        # f = filter(lambda x: x[0] in context_list.keys(), counts)
         # iterate over the counts and set the corresponding indices to the frequency
-        for (context_idx, count) in filter(lambda x: x[0] in words.keys(), counts):
-            vector[context_idx] = count if context_idx in context_list else 0
+        for (context_idx, count) in counts:
+            if dep:
+                context_idx = context_idx[0]
+            vector[context_idx] += count #if context_idx in context_list else 0
         vectors.append(vector)
     vectors = np.array(vectors)
     return vectors
-
 
 def pmi_from_matrix(matrix):
     pmi_matrix = np.zeros(matrix.shape)
@@ -268,17 +268,11 @@ def pmi_from_matrix(matrix):
                 p_y = col_sums[j]
                 pmi = max(0, np.log(p_xy / p_x * p_y) + log_matrix_sum)
                 pmi_matrix[i, j] = pmi
-                # if pmi > 0:
-                #     if i not in sparse_pmi_matrix:
-                #         sparse_pmi_matrix[i] = dict()
-                #     sparse_pmi_matrix[i][j] = pmi
     # normalize row-wise
     pmi_matrix = pmi_matrix / np.linalg.norm(pmi_matrix, axis=1)[:, None]
 
-    # sparse_pmi_matrix = sparse_matrix_to_dict(pmi_matrix)
     sparse_pmi_matrix = csr_matrix(pmi_matrix)
-    rot_matrix = pmi_matrix.T #np.rot90(pmi_matrix, k=-1)
-    # fliped_pmi_matrix = sparse_matrix_to_dict(rot_matrix)
+    rot_matrix = pmi_matrix.T
     fliped_pmi_matrix = csr_matrix(rot_matrix)
 
     return pmi_matrix, sparse_pmi_matrix, fliped_pmi_matrix
@@ -293,22 +287,18 @@ def sparse_matrix_to_dict(matrix):
                 sparse_matrix[i][j] = col
     return sparse_matrix
 
-
 def cosine(v1, v2):
     return np.dot(v1, v2) #/ (np.linalg.norm(v1) * np.linalg.norm(v2))
 
-
 # calculate the similairy vector for word u
 def cosine_with_sparse_matrix(matrix, fliped_matrix, u):
-    dt = [0]*matrix.shape[0]
+    dt = np.zeros(matrix.shape[0])
     # ATT(u) = fliped_matrix[u] (u'th row of fliped_matrix)
     # W(att) = matrix[att] (u'th row of matrix)
-    for att in fliped_matrix[u].indices:
-        for v in matrix[att].indices:
+    for att in matrix[u].indices:
+        for v in fliped_matrix[att].indices:
             dt[v] +=  matrix[u, att] * fliped_matrix[att, v]
-    # dt.reverse()
     return dt
-
 
 def cosine_similarity_matrix(pmi_matrix):
     cosine_matrix = np.zeros(pmi_matrix.shape)
@@ -317,15 +307,14 @@ def cosine_similarity_matrix(pmi_matrix):
             cosine_matrix[i, j] = cosine(row1, row2)
     return cosine_matrix
 
-
-def calc_pmi(context_list, counts, words):
-    context_list = {k: v for k, v in context_list.items() if v >= 75}
-    counts_matrix = vectorize_counts(counts, words, context_list)
+def calc_pmi(context_list, counts, words, word_to_idx, dep=False):
+    # context_list = {k: v for k, v in context_list.items() if v >= 75}
+    counts_matrix = vectorize_counts(counts, words, context_list, word_to_idx, dep=dep)
+    print(f"shape of co-occurence matrix: {counts_matrix.shape}")
     pmi_matrix, sparse_pmi_matrix, fliped_pmi_matrix = pmi_from_matrix(counts_matrix)
     # cosine_matrix = cosine_similarity_matrix(pmi_matrix, words)
     # write_count_to_file(file_name, context_list, idx_to_words)
     return pmi_matrix, sparse_pmi_matrix, fliped_pmi_matrix
-
 
 def k_most_similar(pmi_matrix, flipped_pmi_matrix, u, k=20, with_same=False):
     similarity_with_all = cosine_with_sparse_matrix(pmi_matrix, flipped_pmi_matrix, u)
@@ -345,21 +334,21 @@ def calc_similar_to_words(words, pmi_matrix, flipped_pmi_matrix, word_to_idx):
 def write_to_file_most_similars(similar_full_window, similar_2_window, similar_dep, words_to_test, idx_to_words):
     with open("top20.txt", "w") as f:
         for i, word in enumerate(words_to_test):
-            f.write(word + "\n")
+            f.write(f"{word}\n")
             for x, y, z in zip(similar_2_window[i], similar_full_window[i], similar_dep[i]):
-                f.write(
-                    idx_to_words[x]
-                    + " "
-                    + idx_to_words[y]
-                    + " "
-                    + idx_to_words[z]
-                    + "\n"
-                )
-            f.write("*" * 9 + "\n")
+                f.write(f"{idx_to_words[x]} {idx_to_words[y]} {idx_to_words[z]}\n")
+            f.write(f"{'*' * 9}\n")
 
+def filter_words(words, threshold = 100):
+    new_words = {}
+    for word, count in words.items():
+        if count < threshold:
+            break
+        new_words[word] = count
+    return new_words
 
 def main():
-    words_to_test = ['car','bus','hospital', 'hotel','gun', 'bomb','horse','fox','table','bowl', 'guitar', 'piano']
+    test_words = ['car','bus','hospital', 'hotel','gun', 'bomb','horse','fox','table','bowl', 'guitar', 'piano']
 
     file_name = sys.argv[1]
     df = read_table_from_file(file_name)
@@ -367,35 +356,42 @@ def main():
     # count words
     words = df["LEMMA"].value_counts()
 
-    # mapping word for memory efficiency
     starts_indexes = df.index[df["ID"] == 1].tolist() + [len(df)]
     list_of_sentences = [df.iloc[starts_indexes[n] : starts_indexes[n + 1]] for n in range(len(starts_indexes) - 1)]
+
+    words = filter_words(words, threshold=75)
+    print(f"len of words after filter of 75: {len(words)}")
+    # mapping word for memory efficiency
     idx_to_word = np.array([x[0] for x in words.items()])
     word_to_idx = {word: idx for idx, word in enumerate(idx_to_word)}
-    words = {word_to_idx[word]: count for word, count in words.items() if count >= 100}
+
+    # apply first filter
+    words = filter_words(words, threshold=100)
+    print(f"len of words after filter of 100: {len(words)}")
+    words = {word_to_idx[word]:count for word, count in words.items()}
+
+    words_to_test = [word for word in test_words if word in word_to_idx and word_to_idx[word] in words]
 
     # write words to file
     write_count_to_file("counts_words.txt", words, idx_to_word)
 
-    # words_to_test = ['that', 'by']
-    
     # count context1
-    counts, context_list = context_counter(list_of_sentences, word_to_idx, task="full_window")
-    pmi_matrix_full_window, sparse_matrix_full, fliped_sparse_matrix_full = calc_pmi(context_list, counts, words)
+    word_to_context_counts, context_count = context_counter(list_of_sentences, words, word_to_idx, task="full_window")
+    pmi_matrix_full_window, sparse_matrix_full, fliped_sparse_matrix_full = calc_pmi(context_count, word_to_context_counts, words, word_to_idx)
     similariries_full_window = calc_similar_to_words(words_to_test, sparse_matrix_full, fliped_sparse_matrix_full, word_to_idx)
 
     # count context2
-    counts, context_list2 = context_counter(list_of_sentences, word_to_idx, task="2_window")
-    pmi_matrix_two_window, sparse_matrix_two, fliped_sparse_matrix_two = calc_pmi(context_list2, counts, words)
+    word_to_context_counts2, context_count2 = context_counter(list_of_sentences, words, word_to_idx, task="2_window")
+    pmi_matrix_two_window, sparse_matrix_two, fliped_sparse_matrix_two = calc_pmi(context_count2, word_to_context_counts2, words, word_to_idx)
     similariries_2_window = calc_similar_to_words(words_to_test, sparse_matrix_two, fliped_sparse_matrix_two, word_to_idx)
 
     # count context3
-    counts, context_list3 = context_counter(list_of_sentences, word_to_idx, task="dep_type")
-    pmi_matrix_dep, sparse_matrix_dep, fliped_sparse_matrix_dep = calc_pmi(context_list3, counts, words)
+    word_to_context_counts3, context_count3 = context_counter(list_of_sentences, words, word_to_idx, task="dep_type")
+    write_features_count_to_file("counts_context_dep.txt", context_count3, idx_to_word)
+    pmi_matrix_dep, sparse_matrix_dep, fliped_sparse_matrix_dep = calc_pmi(context_count3, word_to_context_counts3, words, word_to_idx, dep=True)
     similariries_dep = calc_similar_to_words(words_to_test, sparse_matrix_dep, fliped_sparse_matrix_dep, word_to_idx)
 
 
-    write_count_to_file("counts_context_dep.txt", context_list3, idx_to_word)
     write_to_file_most_similars(similariries_full_window, similariries_2_window, similariries_dep, words_to_test, idx_to_word)
 
 
